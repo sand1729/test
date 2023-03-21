a09e4584ea2778ff926e2a6f6c748b2c89d7bced
sand1729 <lekkala1729@gmail.com>
2023-03-21 14:32:21 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Vision Engage is intended to engage patients during and after the studies',
+    author='Vision Engage',
+    license='',
+)
